fb5a128824c8e4be00f3e85e87085da9157b7ad9
Mary Jack <maryjackinthebox@gmail.com>
2018-04-24 20:22:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,11 @@ class Song
   def self.find_by_name(song_name)
     @@all.detect{|song| song.name == song_name}
   end
+  
+  
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+ end
+
+
 end
